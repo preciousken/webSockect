249ca9cad8d484c1a0960ab04aed68686f21cde8
preciousken <pkehinde27@gmail.com>
2023-03-07 16:21:01 +0100

    websocket

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('welcome to exploring webSocket')
+// Create WebSocket connection.
+const socket = new WebSocket('ws://localhost:8080');
+
+// Connection opened
+socket.addEventListener('open', (event) => {
+    socket.send('Hello Server!');
+});
+
+// Listen for messages
+socket.addEventListener('message', (event) => {
+    console.log('Message from server ', event.data);
+});
