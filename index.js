4c194f81bdaa217af2172efcaa29a9602b7b5eee
preciousken <pkehinde27@gmail.com>
2023-03-07 16:26:51 +0100

    websocket

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ socket.addEventListener('open', (event) => {
     socket.send('Hello Server!');
 });
 
-
-
 // Listen for messages
 socket.addEventListener('message', (event) => {
     console.log('Message from server ', event.data);
