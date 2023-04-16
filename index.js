e9fb92df0e1675433b47bc3155fcfde0dba0435d
preciousken <pkehinde27@gmail.com>
2023-04-16 14:30:09 +0100

    continued the socket.io exploration

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-// Create WebSocket connection.
-const socket = new WebSocket('ws://localhost:8080');
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path')
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-// Connection opened
-socket.addEventListener('open', (event) => {
-    socket.send('Hello Server!');
+app.get('/', (req, res) => {
+//   res.send('<h1>Hello world</h1>');
+res.sendFile(path.join(__dirname,'views/index.html'))
 });
-// Listen for messages
-socket.addEventListener('message', (event) => {
-    console.log('Message from server ', event.data);
+
+app.get('*',(req,res)=>{
+    res.json({
+        message:"Just like that! YOu totally missed your way😂🤣"
+    })
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
+  
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
-// 
+
