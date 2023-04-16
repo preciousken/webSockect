ef986ce1edfcc1087788fb56f5d1b7c7112e04a0
preciousken <pkehinde27@gmail.com>
2023-04-16 14:49:10 +0100

    updated the repo

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const http = require('http');
 const path = require('path')
 const server = http.createServer(app);
+
+// WebSocket importing for use
 const { Server } = require("socket.io");
 const io = new Server(server);
 
@@ -19,6 +21,16 @@ app.get('*',(req,res)=>{
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+
+
   });
   
 server.listen(3000, () => {
